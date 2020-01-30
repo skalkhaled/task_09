c1fd8ded7becdf9aad3ec54ef1c2323f0a86acd5
Sana AlKhaled <sanaalkhaled@Sanas-MacBook-Pro.local>
2020-01-30 10:59:26 +0300

    Task9Done

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -1,5 +1,21 @@
 from django import forms
 from .models import Restaurant
+from django.contrib.auth.models import User
+
+
+class SignupForm(forms.ModelForm):
+	class Meta:	
+		model = User
+		fields = ['username', 'first_name', 'last_name','email','password']
+
+		widgets = {
+			'password': forms.PasswordInput()
+		}
+
+class SigninForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, widget = forms.PasswordInput())
+
 
 class RestaurantForm(forms.ModelForm):
     class Meta:
